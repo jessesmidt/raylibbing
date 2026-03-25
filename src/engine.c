a60c341ee1cfc9edf757dcfa30ea3fe58b2c0792
jessesmidt <jessesmidt2002@gmail.com>
2026-03-26 00:36:52 +0100

    the cube moves

--- src/engine.c
+++ src/engine.c
@@ -1,19 +1,41 @@
 #include "raylib.h"
 #include "engine.h"
 
+// Player
+
+typedef struct {
+    Vector3 position;
+    Vector3 velocity;
+    Color   color;
+} Player;
+
+void UpdatePlayer(Player *p)
+{
+    p->position.x += p->velocity.x;
+}
+
+void DrawPlayer(Player p)
+{
+    DrawCube(p.position, 1, 1, 1, BLUE);
+}
+
+// Game Object (used for floor)
+
 typedef struct {
     Vector3 position;
     Vector3 size;
-    Color color;
+    Color   color;
 } GameObject;
 
-void DrawGameObject(GameObject obj) {
+void DrawGameObject(GameObject obj)
+{
     DrawCube(obj.position, obj.size.x, obj.size.y, obj.size.z, obj.color);
 }
 
 typedef struct {
-    Camera3D camera;
-    GameObject floor;
+    Camera3D    camera;
+    GameObject  floor;
+    Player      cube;
 } EngineState;
 
 static EngineState state;
@@ -31,6 +53,12 @@ void init() {
         .color    = LIGHTGRAY
     };
 
+    state.cube = (Player){
+        .position = (Vector3){-4, 2, 0},
+        .velocity = (Vector3){0.05f, 0.0f, 0.0f},
+        .color    = RED
+    };
+
     InitWindow(800, 600, "raylib + python");
 }
 
@@ -41,6 +69,9 @@ void update(EngineState *e)
     DrawText("hello from C", 200, 200, 20, BLACK);
     BeginMode3D(state.camera);
     DrawGameObject(state.floor);
+    DrawPlayer(state.cube);
+    UpdatePlayer(&state.cube);
+    DrawFPS(10, 10);
 
     EndMode3D();
     EndDrawing();
